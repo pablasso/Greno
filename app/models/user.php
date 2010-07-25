3fa3078327b480c7514d89fc690ecc0dda937eab
pablasso <pablasso@gmail.com>
2010-07-25 16:24:39 -0500

    Basic scaffolding for Users

---
+++ app/models/user.php
@@ -0,0 +1,18 @@
+<?php
+class User extends AppModel {
+
+	var $name = 'User';
+
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $belongsTo = array(
+		'Group' => array(
+			'className' => 'Group',
+			'foreignKey' => 'group_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+}
+?>
